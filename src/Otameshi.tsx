fecb5503de1daf59fa94f8de767dd4da219c8d48
minddrop <ryooooooo1@gmail.com>
2020-02-07 23:16:41 +0900

    immutable response object

--- src/Otameshi.tsx
+++ src/Otameshi.tsx
@@ -1,20 +1,61 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
+
+import { VolumeList } from './models/Volume'
+
+const searchGoogleBooks = async (searchString: string): Promise<any> => {
+  const url = 'https://www.googleapis.com/books/v1/volumes'
+  const params = { q: searchString }
+  try {
+    const response = await axios.get(url, { params })
+    return { isSuccess: true, data: response.data, error: null }
+  } catch (error) {
+    return { isSuccess: false, data: null, error }
+  }
+}
 
 export const Otameshi: React.FC = () => {
-  const [text, changeText] = useState('')
+  const [searchString, changeSearchString] = useState('')
+  const [searchResult, changeSearchResult] = useState<VolumeList>(
+    new VolumeList()
+  )
+
+  const handleOnSearchButton = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): Promise<any> => {
+    event.preventDefault()
+    const result = await searchGoogleBooks(searchString)
+    if (result.isSuccess)
+      changeSearchResult(VolumeList.fromResponse(result.data))
+    else window.alert(String(result.error))
+  }
 
   return (
     <Wrapper>
       <Body>
-        <Title>Otameshi</Title>
-        <TextArea
-          placeholder="enter text!"
-          onChange={(event): void => changeText(event.target.value)}
-        >
-          {text}
-        </TextArea>
-        <TextResult>{text}</TextResult>
+        <Title>Google Books Search</Title>
+        <SearchForm>
+          <Input
+            placeholder="検索ワードを入力してね！"
+            onChange={event => changeSearchString(event.target.value)}
+          />
+          <SearchButton
+            onClick={event => handleOnSearchButton(event)}
+            disabled={!searchString}
+          >
+            Search
+          </SearchButton>
+        </SearchForm>
+        {searchResult && (
+          <ResultContent>
+            {searchResult.items.map((item: any) => {
+              return (
+                <ResultTitle key={item.id}>{item.volumeInfo.title}</ResultTitle>
+              )
+            })}
+          </ResultContent>
+        )}
       </Body>
     </Wrapper>
   )
@@ -23,26 +64,58 @@ export const Otameshi: React.FC = () => {
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
+  margin-top: 20px;
 `
 
 const Body = styled.div``
 
 const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
   text-align: center;
 `
 
-const TextArea = styled.textarea`
+const Input = styled.input`
   display: block;
-  margin: 0 auto;
   box-sizing: border-box;
-  width: 200px;
+  width: 250px;
+  font-size: 18px;
+  padding: 10px;
+  outline: none;
 `
 
-const TextResult = styled.p`
-  width: 200px;
+const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`
+
+const SearchButton = styled.button`
+  color: #fff;
+  background-color: #09d3ac;
+  border-radius: 3px;
+  margin-left: 10px;
   padding: 10px;
-  margin: 20px;
-  border: 1px solid blue;
-  white-space: pre-wrap;
-  box-sizing: border-box;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  transition: 0.4s;
+  cursor: pointer;
+  &:disabled {
+    background-color: #bfbfbf;
+    cursor: not-allowed;
+  }
+`
+
+const ResultContent = styled.div`
+  margin-top: 20px;
+`
+
+const ResultTitle = styled.div`
+  padding: 10px 0;
+  border-bottom: 1px solid;
+  &:first-of-type {
+    border-top: 1px solid;
+  }
 `
